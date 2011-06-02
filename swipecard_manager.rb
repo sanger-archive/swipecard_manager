68098bddb8fb37059772400019b87ff444d77a9e
Maxime Bourget <mb14@sanger.ac.uk>
2011-06-02 16:45:03 +0100

    Add update page

--- swipecard_manager.rb
+++ swipecard_manager.rb
@@ -5,6 +5,8 @@ require 'sinatra'
 load 'sanger_authentication.rb'
 require 'configatron'
 require 'haml'
+#require 'sequencescape-api'
+require 'sequencescape'
 
 
 APP_ROOT = File.dirname(File.expand_path(__FILE__))
@@ -28,19 +30,42 @@ init
 def find_login_from_cookies(cookies)
   halt 500, "server not configured" unless @@sanger_authentication
   halt 500, "cookie name not configured" unless token_name=configatron.authentication_service.cookie_name
-  token = cookies[token_name]
+  @sso_token = cookies[token_name]
   #halt 301, "please log on " unless token
-  login = @@sanger_authentication.sso_login_from_cookie(token)
-  if token && !login
+  login = @@sanger_authentication.sso_login_from_cookie(@sso_token)
+  if @sso_token && !login
     halt 500, "Authentication service error"
   else
     login
   end
 end
 
+def api
+  @api ||= begin
+             sequencescape_url = configatron.sequencescape.url
+             halt 500, "Serves missconfigured. sequencescape.url required" unless sequencescape_url
+             Sequencescape::Api.new(:url => sequencescape_url, :cookie => @sso_token, :authorisation => configatron.sequencescape.authorisation)
+           end
+end
+
+def find_user_by_login(login)
+  searcher = api.search.find(configatron.sequencescape.find_user_by_login_uuid || halt("500", "Server missconfigured, please a sequencescape_uuid"))
+  searcher.first(:login => login)
+end
+
+def update_code(name, code)
+  user = find_user_by_login(name)
+  user.swipecard_code=code
+  user.save!
+end
+
 # find user from cookie
 before do 
   @user = find_login_from_cookies(request.cookies)
+end
+
+def check_user_logged_in
+    redirect to('/login') unless @user
 end
 # main page
 get '/' do
@@ -48,15 +73,17 @@ get '/' do
 end
 
 get '/code_input' do
-  case @user
-  when nil
-    redirect to('/login')
-  else
-    haml :update
-  end
+  check_user_logged_in
+  haml :code_input
 end
 
 post '/update' do
+  check_user_logged_in
+  user = @user 
+  code = params["code"]
+
+  @success = update_code(user, code)
+  haml :update
 end
 
 get '/login' do
